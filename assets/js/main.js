b8c7b2a98de89d92ccb7bab5e396b3ce3167be71
Alexander Ceron <edwin.ceron@hotmail.com>
2017-05-27 12:12:20 -0500

    mm

--- assets/js/main.js
+++ assets/js/main.js
@@ -54,7 +54,8 @@ jQuery(document).ready(function($) {
         }
     );
     
-    
+    /* Github Calendar - https://github.com/IonicaBizau/github-calendar */
+    GitHubCalendar("#github-graph", "IonicaBizau");
     
     
     /* Github Activity Feed - https://github.com/caseyscarborough/github-activity */
